4eca25977c5ebb8752913b4d7f6a058917be20b3
Cheolsoon <mn5724@naver.com>
2021-09-10 20:49:06 +0900

    fix main branch

--- README.md
+++
@@ -1,2 +0,0 @@
-# Literacy-Improvement-Web
-문해력 향상을 돕는 웹사이트
--- src/redux/signup/types.js
+++
@@ -1,4 +0,0 @@
-// Register
-export const REGISTER = "REGISTER";
-export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
-export const REGISTER_FAILURE = "REGISTER_FAILURE";
